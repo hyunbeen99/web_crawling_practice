4c281735e496b5f1c31f8273bfab4c229465239e
hyunbeen99 <gusqls1124@gmail.com>
2020-11-10 02:13:49 +0900

    Change code to def

--- web_crawling.py
+++ web_crawling.py
@@ -1,21 +1,29 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
-bsObject = BeautifulSoup(html, "html5lib")
+def findTags(url):
+
+	bs = urlopen(url)
+	bsObject = BeautifulSoup(bs, "html5lib")
+
+	### findAll(tag, attributes, recursive, text, limit, keywords) ###
+	### if recursive == True -> find son of this tag, default = true
+	### if recursive == False -> find only top of this tag
+
+	nameSpan = bsObject.findAll('span', {'class' : 'green'}) #findAll(tageName, tagAttribute)
+	#nameList = bsObject.findAll('span')
+	#nameList = bsObject.findAll('span', {'class' : 'green','red'}) #findAll(tageName, tagAttribute)
+	nameList = bsObject.findAll(text = "the prince")
+	print(len(nameList))
+
+	for name in nameSpan:
+		a = name.get_text()
+
+	return a
+
+answer = findTags("http://www.pythonscraping.com/pages/warandpeace.html")
 
 
-### findAll(tag, attributes, recursive, text, limit, keywords) ###
-### if recursive == True -> find son of this tag, default = true
-### if recursive == False -> find only top of this tag
-
-nameSpan = bsObject.findAll('span', {'class' : 'green'}) #findAll(tageName, tagAttribute)
-#nameList = bsObject.findAll('span')
-#nameList = bsObject.findAll('span', {'class' : 'green','red'}) #findAll(tageName, tagAttribute)
-nameList = bsObject.findAll(text = "the prince")
-print(len(nameList))
-
-for name in nameSpan:
-	print(name.get_text())
-
-
+def codeForTree(url):
+#bs.tag.subTag.anotherSubTag
+	
