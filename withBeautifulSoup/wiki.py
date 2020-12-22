2cbbd3f0f0512fec6f7b8815932954ac3f9c3520
hyunbeen99 <you@example.com>
2020-12-22 19:31:19 +0900

    Practice with beautifulSoup

---
+++ withBeautifulSoup/wiki.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://en.wikipedia.org/wiki/Kevin_Bacon')
+bs = BeautifulSoup(html, 'html.parser')
+
+#attr in findAll object
+for link in bs.findAll('a'):
+	if 'href' in link.attrs:
+		print(link.attrs['href'])
+
